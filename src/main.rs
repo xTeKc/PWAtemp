b78cf7602e1cb2b3eee287b230b767de55179460
xTeKc <bcd3v@pm.me>
2021-10-04 22:25:30 -0400

    print data

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ use std::env::var;
 fn main() {
     let _aid = var("API_ID").unwrap_or("none".to_string());
     let _su = var("SERVER_URL").unwrap_or("none".to_string());
+
+    println!("{} {}", _aid, _su);
 }
