b234c17d990abbcf43fffefb8bf8dc1a372446bd
xTeKc <bcd3v@pm.me>
2021-10-05 23:02:01 -0400

    change name of var and modify

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use std::fs;
 use std::io::Error;
 
 
-fn main() {
+fn main() -> Result<(), Error> {
+    // read `config_path` from the environment variable `CONFIG`.
+    // If `CONFIG` isn't set, fall back to a default config path.
+    let env_path = env::var(".env");
+
     
+    
+    Ok(())
 }
