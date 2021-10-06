1d897909d2232f153c3a800a011ff235a3938d7d
xTeKc <bcd3v@pm.me>
2021-10-05 22:12:15 -0400

    clear

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use std::env;
+
+
 
 fn main() {
-    // let _aid = var("API_ID").unwrap_or("none".to_string());
-    // let _su = var("SERVER_URL").unwrap_or("none".to_string());
-    // println!("{} {}", _aid, _su);
-
-    let _aid = "API_ID";
-    match env::var(_aid) {
-        Ok(val) => println!("{}: {:?}", _aid, val),
-        Err(e) => println!("couldn't interpret {}: {}", _aid, e),
-    }
+    
 }
