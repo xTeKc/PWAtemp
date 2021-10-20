ca4ff8c249bb2203db7ba1465e08e9b6e7e0fded
xTeKc <bcd3v@pm.me>
2021-10-20 13:48:03 -0400

    update names and change bg color

--- src/main.rs
+++ src/main.rs
@@ -40,28 +40,19 @@
 //     }
 // }
 
-use iced::{
-    Element,
-    Sandbox,
-    Settings,
-    Text
-};
+use iced::{Color, Element, Sandbox, Settings, Text};
 
-pub fn main() -> iced::Result {
-    Hello::run(Settings::default())
-}
+struct Browser;
 
-struct Hello;
-
-impl Sandbox for Hello {
+impl Sandbox for Browser {
     type Message = ();
 
-    fn new() -> Hello {
-        Hello
+    fn new() -> Browser {
+        Browser
     }
 
     fn title(&self) -> String {
-        String::from("A cool application")
+        String::from("PWA Browser")
     }
 
     fn update(&mut self, _message: Self::Message) {
@@ -69,6 +60,14 @@ impl Sandbox for Hello {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello, world!").into()
+        Text::new("Browser").into()
     }
+
+    fn background_color(&self) -> iced::Color {
+        Color::BLACK
+    }
+}
+
+pub fn main() -> iced::Result {
+    Browser::run(Settings::default())
 }
