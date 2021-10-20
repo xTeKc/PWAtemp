c4ff67546c6db6431694c6966dde18f5f212e954
xTeKc <bcd3v@pm.me>
2021-10-20 13:38:58 -0400

    simple setup hw

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,74 @@
-use iced::*; 
+// use iced::{
+//     button,
+//     Button,
+//     Column,
+//     Sandbox,
+//     Settings,
+// }; 
 
-struct Search {
-    // search button value
-    value: String,
+// struct Search {
+//     // search button value
+//     value: String,
 
-   // local state of search button
-   search_button: button::State
+//    // local state of search button
+//    search_button: button::State
+// }
+
+// // user interactions for search
+// #[derive(Debug, Clone, Copy)]
+// pub enum Message {
+//     SearchPressed
+// }
+
+// // implement with view logic
+// impl Search {
+//     pub fn view(&mut self) -> Column<Message> {
+//         Column::new()
+//             .push(
+//                 // produces the message when pressed
+//                 Button::new(&mut self.search_button, Text::new("^"))
+//                         .on_press(Message::SearchPressed),
+//             )
+//     }
+
+//     pub fn update(&mut self, message: Message) {
+//         match message {
+//             Message::SearchPressed => {
+//                 self.value = "Search".to_string();
+//             }
+//         }
+//     }
+// }
+
+use iced::{
+    Element,
+    Sandbox,
+    Settings,
+    Text
+};
+
+pub fn main() -> iced::Result {
+    Hello::run(Settings::default())
 }
 
-// user interactions for search
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-    SearchPressed
-}
+struct Hello;
 
-// implement with view logic
-impl Search {
-    pub fn view(&mut self) -> Column<Message> {
-        Column::new()
-            .push(
-                // produces the message when pressed
-                Button::new(&mut self.search_button, Text::new("^"))
-                        .on_press(Message::SearchPressed),
-            )
+impl Sandbox for Hello {
+    type Message = ();
+
+    fn new() -> Hello {
+        Hello
     }
 
-    pub fn update(&mut self, message: Message) {
-        match message {
-            Message::SearchPressed => {
-                self.value = "Search".to_string();
-            }
-        }
+    fn title(&self) -> String {
+        String::from("A cool application")
     }
-}
 
-fn main() {
+    fn update(&mut self, _message: Self::Message) {
+        // This application has no interactions
+    }
 
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
 }
