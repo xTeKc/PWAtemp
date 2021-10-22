37ee685077c1870c3179dd0cebd5c67075276879
xTeKc <bcd3v@pm.me>
2021-10-22 15:47:10 -0400

    reset

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,6 @@
-// use iced::{Color, Element, Sandbox, Settings, Text};
+use egui;
 
-// struct Browser;
 
-// impl Sandbox for Browser {
-//     type Message = ();
+fn main() {
 
-//     fn new() -> Browser {
-//         Browser
-//     }
-
-//     fn title(&self) -> String {
-//         String::from("PWA Browser")
-//     }
-
-//     fn update(&mut self, _message: Self::Message) {
-//         // This application has no interactions
-//     }
-
-//     fn view(&mut self) -> Element<Self::Message> {
-//         Text::new("Browser").into()
-//     }
-
-//     fn background_color(&self) -> iced::Color {
-//         Color::BLACK
-//     }
-    
-    
-
-// }
-
-// pub fn main() -> iced::Result {
-//     Browser::run(Settings::default())
-// }
+}
