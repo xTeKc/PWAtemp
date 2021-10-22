b88138a0020ef1c012350d1f1e19bdf10d0fe0f6
xTeKc <bcd3v@pm.me>
2021-10-22 16:06:31 -0400

    refactor window struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use egui;
+use egui::{self, Area, Frame, Label, Resize, ScrollArea};
 
 
-pub struct Window {
-    
+pub struct Window<'open> {
+    title_label: Label,
+    open: Option<&'open mut bool>,
+    area: Area,
+    frame: Option<Frame>,
+    resize: Resize,
+    scroll: Option<ScrollArea>,
+    collapsible: bool,
+    with_title_bar: bool,
 }
 
 fn main() {
