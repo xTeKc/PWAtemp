ad6f918c8c08e821382896a8b89bcf1883ec95e7
xTeKc <bcd3v@pm.me>
2021-10-23 22:59:42 -0400

    clear

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
-use egui::{self, Area, Color32, Frame, Id, Label, Order, Resize, ScrollArea};
+use egui::*;
 
 
-pub struct Window<'open> {
-    title_label: Label,
-    open: Option<&'open mut bool>,
-    area: Area,
-    frame: Option<Frame>,
-    resize: Resize,
-    scroll: Option<ScrollArea>,
-    collapsible: bool,
-    with_title_bar: bool,
-}
-
-pub struct LayerId {
-    pub order: Order,
-    pub id: Id,
-}
 
 fn main() {
     
